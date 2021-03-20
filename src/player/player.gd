d2bdef6364dc8f4cd1050685b3b5b88e50d33f1b
Fupi <fupicat@gmail.com>
2021-03-20 11:45:53 -0300

    de-yanderedevify

--- src/player/player.gd
+++ src/player/player.gd
@@ -4,30 +4,31 @@ var move = Vector2()
 var life = 3
 var walking = false
 var percentage = 0
+var speed = 300
 
 func _process(delta):
-    # Isso dói em mim mas é o que tem pra hoje
-    var speed = 300 * delta
-    if Input.is_action_pressed("ui_up"):
-        position.y -= speed
-    if Input.is_action_pressed("ui_down"):
-        position.y += speed
-    if Input.is_action_pressed("ui_left"):
-        position.x -= speed
-    if Input.is_action_pressed("ui_right"):
-        position.x += speed
+    move = Vector2(0, 0)
+    walking = false
+    
+    move.y = int(Input.is_action_pressed("ui_down")) - int(Input.is_action_pressed("ui_up"))
+    move.x = int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left"))
+    
+    if move != Vector2(0, 0):
+        walking = true
+    
+    move = move * speed
+    
     move.normalized()
     move_and_slide(move)
-
-func _input(event):
+    
     #Quando apertar E, uma barra circular irá aparecer. Se houver algum movimento, a barra sumirá e parará de acrescentar números.
     if !walking && Input.is_action_pressed("ui_accept"):
+        if !$textureprogress.visible:
+            $anim.stop(true)
+            $anim.play("fade_in")
         percentage += 0.1
         $textureprogress.value = percentage
-        $textureprogress.visible = true
         if $textureprogress.value >= 100: # Quando completar 100, o jogo fechará.
             get_tree().quit()
     else:
         $textureprogress.visible = false
-    if !walking && Input.is_action_just_pressed("ui_accept"):
-        $anim.play("fade_in")

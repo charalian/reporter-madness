33f75850662e0a7e44def0b0d519d851e97965e2
gabrielc <62511047+charalian@users.noreply.github.com>
2021-03-21 11:23:38 -0300

    some organization and separated a func

--- src/enemy/enemy.gd
+++ src/enemy/enemy.gd
@@ -1,25 +1,26 @@
 extends Living
 
 onready var player = $"../player"
+
 var life = 1
-var int_player = false # Um sim/não de estiver tocando no player
+var int_player = false # When touching player is true/false
 
 func _init():
     speed = 100
 
 func mov_custom():
-    if !int_player: # Se int_player for falso, mova-se típicamente.
+    if !int_player:
         var player_pos = player.position
         var prev_rot = rotation
         look_at(player_pos)
         move = Vector2(speed, 0).rotated(rotation)
         rotation = prev_rot
-    elif int_player: # Se int_player for verdadeiro, deixe inanimado.
+    elif int_player:
         move = Vector2.ZERO
 
 func _on_area2d_body_entered(body):
-    if body.is_in_group("player") && !int_player: # Quando colidir com player
-        int_player = true # Mude int_player para verdadeiro
+    if body.is_in_group("player") && !int_player:
+        int_player = true 
 
-func on_death(): # Quando morrer, queue_free().
+func on_death():
     queue_free()
